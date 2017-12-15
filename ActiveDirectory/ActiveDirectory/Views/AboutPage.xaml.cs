3f381921f41163dc7e9e2141b6cafbba3b37133e
Laurent Greyling <l.greyling@niposoftware.com>
2017-12-15 11:26:36 +0100

    Add project files.

---
+++ ActiveDirectory/ActiveDirectory/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ActiveDirectory
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
